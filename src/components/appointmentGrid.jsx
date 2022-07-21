dae771ce8dd24f8b033d22bbe150c8f11cd1ad63
Dhruv Singla <singladhruv18@gmail.com>
2022-07-21 22:31:37 +0530

    tooltip

--- src/components/appointmentGrid.jsx
+++ src/components/appointmentGrid.jsx
@@ -3,26 +3,16 @@ import AppointmentCard from './designs/AppointmentCard'
 
 export default function AppointmentGrid() {
   return (
-    <div className={`grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 m-5 duration-300 ease-in`}>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                <AppointmentCard/>
-                
+    <div className={`grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 m-5 duration-300 ease-in`}>
+              <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
+              <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
+              <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
+              <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
+              <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
+              <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
+              <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
+              <AppointmentCard title='this is a test meeting' time='9:11 AM'/>
+              <AppointmentCard title='this is a test meeting' time='9:11 AM'/>               
             </div>
   )
 }
